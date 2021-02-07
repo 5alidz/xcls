2a80d6bc25368a4843a33dbdba21093bfce95685
khaled <kzack-dev@hotmail.com>
2021-02-07 04:46:51 +0300

    refactor & update git hooks

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
 export type ClassName = string | null | undefined;
 export type ClassNameArg = ClassName | [boolean, ClassName, ClassName?];
 
+const pad = (s: string) => s.trim() + ' ';
+
 export default function cls(...classesToMerge: ClassNameArg[]): string {
   let result = '';
+
   for (let i = 0; i < classesToMerge.length; i++) {
     const current = classesToMerge[i];
-    const padding = ' ';
-    if (current && typeof current === 'string') {
-      result += current.trim() + padding;
+    if (!current) continue; // ignore any valsy value, also skips over empty string;
+
+    if (typeof current == 'string') {
+      result += pad(current);
       continue;
     }
+
     if (Array.isArray(current)) {
       const [condition, className, fallback] = current;
       if (condition && className && typeof className === 'string') {
-        result += className.trim() + padding;
+        result += pad(className);
       }
       if (!condition && fallback && typeof fallback === 'string') {
-        result += fallback.trim() + padding;
+        result += pad(fallback);
       }
     }
   }
+
   return result.trim();
 }
